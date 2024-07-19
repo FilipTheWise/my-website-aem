3608f145f9146ca35105dc5a3f5d51be1f3b87c2
FilipTheWise <filip.novak2201@gmail.com>
2024-07-19 16:04:03 +0200

    New block

---
+++ blocks/quote/quote.js
@@ -0,0 +1,42 @@
+function hasWrapper(el) {
+    return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
+
+}
+
+export default async function decorate(block) {
+
+    const [quotation, attribution] = [...block.children].map((c) => c.firstElementChild);
+    //console.log(quotation);
+   
+    const blockquote = document.createElement('blockquote');
+    
+    quotation.className = 'quote-quotation';
+
+    if (!hasWrapper(quotation)) {
+        quotation.innerHTML = `<p>${quotation.innerHTML}</p>`;
+    }
+
+    blockquote.append(quotation)
+
+
+    if (attribution) {
+        attribution.className = 'quote-attribution';
+        if (!hasWrapper(attribution)) {
+          attribution.innerHTML = `<p>${attribution.innerHTML}</p>`;
+        }
+        blockquote.append(attribution);
+        
+        const ps = attribution.querySelectorAll('p');
+        //console.log(ps);
+        ps.forEach((p) => {
+          const cite = document.createElement('cite');
+          cite.innerHTML = p.innerHTML;
+          p.replaceWith(cite);
+        });
+      }
+    //console.log(blockquote);
+
+
+    block.innerHTML = '';
+    block.append(blockquote);
+}
